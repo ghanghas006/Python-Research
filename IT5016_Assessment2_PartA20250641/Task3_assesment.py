085cc24267a7f17f00b92451de54bb07e81536ae
ghanghas006 <roshughanghas@gmail.com>
2025-05-30 19:52:57 +1200

    Update Task3_assesment.py

--- IT5016_Assessment2_PartA20250641/Task3_assesment.py
+++ IT5016_Assessment2_PartA20250641/Task3_assesment.py
@@ -7,7 +7,7 @@ Author: Roshi
 from Task1_assesment import staff_info
 from Task2_assesment import requisition_total
 
-# import the approval function
+# defining the approval function
 def requisition_approval(total_cost, staff_id, requisition_id):
     status = "Approved" if total_cost > 0 and total_cost < 500 else "Pending"
     ref_number = staff_id[:3].upper() +str(requisition_id)[-3:]
