788c72a5f03bcfd5a9b073e9d2495d6e352df60d
ghanghas006 <roshughanghas@gmail.com>
2025-05-30 19:00:40 +1200

    Update task2_addons.py

--- Booking.py/task2_addons.py
+++ Booking.py/task2_addons.py
@@ -1,12 +1,14 @@
 def select_tickets_and_addons():
     options = []
     total_cost = 0
-
+    
+    #asking about the ticket type 
     print("\nTICKET TYPES & ADD_ONS")
     while True:
         name = input("Enter ticket type/add-on (e.g., VIP, Standard, Lightstick, Album) or 'done' to finish: ")
         if name.lower() == 'done':
             break
+        #price for ticket
         try:
             price = float(input(f"Enter the price for '{name}': $"))
             options.append((name, price))
