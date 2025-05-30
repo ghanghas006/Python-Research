0abc00eafce8a03fedbf04ae465784f719e5bad5
ghanghas006 <roshughanghas@gmail.com>
2025-05-30 19:12:25 +1200

    Update task3_approval.py

--- Booking.py/task3_approval.py
+++ Booking.py/task3_approval.py
@@ -1,5 +1,6 @@
+#defining the function
 def approve_booking(total_cost, ticket_id, id_number):
-    #information about the ticket status
+    # if and else statement is used
     status = "Confirmed" if total_cost > 0 else "Pending"
     ref_number = id_number[:3].upper() + str(ticket_id)[-2:]
     return status, ref_number
