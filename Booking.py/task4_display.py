4a42bec25390eb4290638f8f0a5ce8e10f4c37cc
ghanghas006 <roshughanghas@gmail.com>
2025-05-30 19:06:32 +1200

    Update task4_display.py

--- Booking.py/task4_display.py
+++ Booking.py/task4_display.py
@@ -1,3 +1,4 @@
+#importing the information from the other tasks
 from task1_booking import concert_booking
 from task2_addons import select_tickets_and_addons
 from task3_approval import approve_booking
@@ -5,10 +6,12 @@ from task3_approval import approve_booking
 def display_concert_booking():
     id_counter = 5000
 
+    #last tasks deatils
     id_counter, ticket_id, id_number, fan_id, fan_name, tour_city = concert_booking(id_counter)
     selected_options, total_cost = select_tickets_and_addons()
     status, ref_number = approve_booking(total_cost, ticket_id,id_number)
 
+    #printing concert 
     print("\n YOUR CONCERT BOOKING RECEIPT")
     print(f"Ticket ID: {ticket_id}")
     print(f"Name: {fan_name}")
